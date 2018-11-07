efdd393b9f6e30eb84c1dd75d276c1dae59af4e7
Mikhail Dyomin <mikhaildyomin@gmail.com>
2018-11-07 15:30:38 +0300

    Updates

---
+++ precache-manifest.0297b40d1626aa813b7068fd82e3f393.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b1e0c50159a1fac33d8",
+    "url": "/spark-product-listing-page/static/js/runtime~main.7b1e0c50.js"
+  },
+  {
+    "revision": "9ddc4f9b4dd854c77fbb",
+    "url": "/spark-product-listing-page/static/js/main.9ddc4f9b.chunk.js"
+  },
+  {
+    "revision": "08f17992d566a5c2ad43",
+    "url": "/spark-product-listing-page/static/js/1.08f17992.chunk.js"
+  },
+  {
+    "revision": "9ddc4f9b4dd854c77fbb",
+    "url": "/spark-product-listing-page/static/css/main.8e7d6bfe.chunk.css"
+  },
+  {
+    "revision": "a34ec2455c991c2a65af27ae6d161a9b",
+    "url": "/spark-product-listing-page/index.html"
+  }
+];
